2a6bc91f7f3a476532c9e611b6aaad18f5e6ff64
Erin Yener <Erin Yener>
2017-05-06 16:47:03 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,28 @@
+function _develoment (){
+  return {
+    "mongodb": "mongodb://localhost/testing",
+    "port":8088
+  }
+}
+
+function _production(){
+  if(!process.env.NODE_PASSWORD){
+    throw new Error ("Missing password");
+  }
+  return {
+    "mongodb": "mongodb://localhost/production",
+    "password": process.env.NODE_PASSWORD,
+    "port":9000
+  }
+}
+
+module.exports = function(){
+  switch(process.env.NODE_ENV){
+    case 'development':
+      return _development;
+    case 'production':
+      return _production;
+    default:
+      return _development;
+  }
+}
